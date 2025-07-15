d56b481c6347743b273f27232e722fe4bf232e4e
Rahul Suresh <130126170+slimycashcodes@users.noreply.github.com>
2025-07-15 10:57:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 window.onload = function () {
     const display = document.getElementById("display");
-
+    display.focus();
     window.appendToDisplay = function (input) {
         display.value += input;
     };
+    document.addEventListener("click", () => display.focus());
+
+ window.toggleTheme = function () {
+    const body = document.body;
+    const toggleBtn = document.getElementById("toggle");
+
+    const isDark = body.classList.toggle("dark");
+    body.classList.toggle("white", !isDark); 
+    toggleBtn.textContent = isDark ? "☀️" : "🌙";
+};
 
     window.compute = function () {
         try{
-            display.value = eval(display.value);
+            result = eval(display.value);
+            result = Number(result).toPrecision(10);
+            display.value = parseFloat(result);
         }
         catch (error) {
             console.error(error);
@@ -22,4 +34,27 @@ window.onload = function () {
     window.dell = function(){
         display.value = display.value.slice(0,-1);
     };
+
+    document.addEventListener("keydown",(e)=>{
+        const all =/[0-9+\-*().]/;
+        if(!(all.test(e.key))){e.preventDefault();}
+        if(e.key==="Enter" || e.key==="="){
+            e.preventDefault();
+            compute();
+        }
+        
+    });
+    document.addEventListener("keydown",(e)=>{
+        if(e.key=="Backspace"){
+            e.preventDefault();
+            dell();
+        }
+    });
+
+    document.addEventListener("keydown",(e)=>{
+        
+
+        if(!(all.test(e))){e.preventDefault();} 
+    });
 };
+
